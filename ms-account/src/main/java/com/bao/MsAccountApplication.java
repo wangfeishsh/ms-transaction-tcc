4be1fdee55dc48ab202ce8008b6d8a5d9fc27260
nannan <nannan@wywk.cn>
2017-08-21 11:03:07 +0800

    init

---
+++ ms-account/src/main/java/com/bao/MsAccountApplication.java
@@ -0,0 +1,12 @@
+package com.bao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAccountApplication.class, args);
+	}
+}
---
+++ ms-coordinator/src/main/java/com/bao/MsCoordinatorApplication.java
@@ -0,0 +1,12 @@
+package com.bao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCoordinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCoordinatorApplication.class, args);
+	}
+}
